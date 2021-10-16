7cc1bf77b7978128d942cd362f1eaff22a6ea876
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2021-10-16 16:17:52 +0200

    Finish Library

---
+++ Library/Source/Cube.cs
@@ -0,0 +1,89 @@
+﻿namespace Minesweeper_3D_Library
+{
+    /// <summary>
+    /// Contains all acceptable states of a <see cref="Cube"/>.
+    /// </summary>
+    public enum CubeState { Covered, Uncovered, Flagged }
+
+    /// <summary>
+    /// Represents the base unit of the Minesweeper game. (Equivalent to the separate squares/boxes in 2D version)
+    /// </summary>
+    public class Cube
+    {
+        #region Variables and Properties
+
+        /// <summary>
+        /// Gets or sets the <see cref="CubeState"/> of the <see cref="Cube"/>.
+        /// </summary>
+        public CubeState State { get; set; }
+
+        /// <summary>
+        /// Gets (or sets) a value indicating whether there is a mine in the <see cref="Cube"/>.
+        /// </summary>
+        /// <returns>True if there is a mine on the <see cref="Cube"/>, otherwise false.</returns>
+        public bool HasMine { get; protected set; }
+
+        /// <summary>
+        /// Gets (or sets) the amount of mines surrounding the <see cref="Cube"/>.
+        /// </summary>
+        public byte SurroundingMines { get; protected set; }
+
+        /// <summary>
+        /// Gets (or sets) the position of the <see cref="Cube"/>.
+        /// </summary>
+        public int[] Position { get; protected set; } = null;
+
+        /// <summary>
+        /// Gets the <see cref="MineSpace"/> this <see cref="Cube"/> is a part of.
+        /// </summary>
+        protected MineSpace Parent { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Cube"/>.
+        /// </summary>
+        /// <param name="mineSpace">The parent <see cref="MineSpace"/> of this <see cref="Cube"/>. (The <see cref="MineSpace"/> this <see cref="Cube"/> will be a part of).</param>
+        /// <param name="hasMine">A value indicating whether this <see cref="Cube"/> is supposed to have a mine.</param>
+        public Cube(MineSpace mineSpace, bool hasMine)
+        {
+            Parent = mineSpace;
+            HasMine = hasMine;
+            State = CubeState.Covered;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Cube"/>.<br />
+        /// This constructor accepts an additional value (<i>position</i>).<br />
+        /// <see cref="Cube"/>s created using this constructor don't have to find out their position themselves during <see cref="Init"/>. This saves some processing power needed for that operation.
+        /// </summary>
+        /// <param name="mineSpace">The parent <see cref="MineSpace"/> of this <see cref="Cube"/>. (The <see cref="MineSpace"/> this <see cref="Cube"/> will be a part of).</param>
+        /// <param name="hasMine">A value indicating whether this <see cref="Cube"/> is supposed to have a mine.</param>
+        /// <param name="position">The <see cref="Position"/> value of the newly created <see cref="Cube"/>.</param>
+        public Cube(MineSpace mineSpace, bool hasMine, int[] position) : this(mineSpace, hasMine)
+        {
+            Position = position;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes the <see cref="Cube"/> by setting it's <see cref="Position"/> value (if it isn't already set) and <see cref="SurroundingMines"/> value.<br />
+        /// This initialization should be done after the filling of the parent <see cref="MineSpace"/> is complete.<br />
+        /// These values are calculated here so they are immediatelly available at any time, saving the processing power required to find out these parameters each time they are needed.
+        /// </summary>
+        public void Init()
+        {
+            if (Position == null)
+                Position = Parent.GetCubePosition(this);
+
+            SurroundingMines = Parent.GetSurroundingMines(this);
+        }
+
+        #endregion
+    }
+}
---
+++ Library/Source/MineSpace.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace Minesweeper_3D_Library
+{
+    /// <summary>
+    /// Represents the playing area of the Minesweeper game. (Equivalent to the minefield in the 2D version)
+    /// </summary>
+    public class MineSpace
+    {
+        #region Variables and Properties
+
+        /// <summary>
+        /// Gets an array containing all of the <see cref="Cubes"/> in the <see cref="MineSpace"/>.
+        /// </summary>
+        public Cube[,,] Cubes { get; }
+
+        /// <summary>
+        /// Gets the width (the amount of X coordinates) of the <see cref="MineSpace"/>.
+        /// </summary>
+        public int Width { get => Cubes.GetLength(0); }
+
+        /// <summary>
+        /// Gets the height (the amount of Y coordinates) of the <see cref="MineSpace"/>.
+        /// </summary>
+        public int Height { get => Cubes.GetLength(1); }
+
+        /// <summary>
+        /// Gets the depth (the amount of Z coordinates) of the <see cref="MineSpace"/>.
+        /// </summary>
+        public int Depth { get => Cubes.GetLength(2); }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Minespace"/>.
+        /// </summary>
+        /// <param name="width">The width (the amount of X coordinates) of the new <see cref="MineSpace"/>.</param>
+        /// <param name="height">The height (the amount of Y coordinates) of the new <see cref="MineSpace"/>.</param>
+        /// <param name="depth">The depth (the amount of Z coordinates) of the new <see cref="MineSpace"/>.</param>
+        /// <param name="mineCount">The amount of mines this new minespace should contain.</param>
+        public MineSpace(int width, int height, int depth, int mineCount)
+        {
+            Cubes = new Cube[width, height, depth];
+
+            FillField(mineCount);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void FillField(int mineCount)
+        {
+            FillMines(mineCount);
+
+            for (int z = 0; z < Depth; z++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < Width; x++)
+                    {
+                        if (Cubes[x, y, z] == null)
+                            Cubes[x, y, z] = new Cube(this, false, new int[] { x, y, z });
+                    }
+                }
+            }
+        }
+
+        private void FillMines(int mineCount)
+        {
+            Random random = new Random();
+            int limit = mineCount < Width * Height * Depth ? mineCount : Width * Height * Depth;
+
+            int[] coords;
+            for (int i = 0; i < limit; i++)
+            {
+                coords = new int[] { random.Next(0, Width), random.Next(0, Height), random.Next(0, Depth) };
+
+                if (Cubes[coords[0], coords[1], coords[2]] == null)
+                    Cubes[coords[0], coords[1], coords[2]] = new Cube(this, true, new int[] { coords[0], coords[1], coords[2] });
+                else
+                    i--;
+            }
+        }
+
+        /// <summary>
+        /// Finds the position of the given <see cref="Cube"/> in the <see cref="Cubes"/> array.
+        /// </summary>
+        /// <param name="cube">The <see cref="Cube"/> to find.</param>
+        /// <returns>An array of <see cref="int"/> containing the [x,y,z] coordinates of the given <see cref="Cube"/>, or <see cref="null"/> if the <see cref="Cube"/> is not found.</returns>
+        public int[] GetCubePosition(Cube cube)
+        {
+            for (int z = 0; z < Depth; z++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < Width; x++)
+                    {
+                        if (Cubes[x, y, z] == cube)
+                            return new int[] { x, y, z };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the amount of mines surrounding a given position.
+        /// </summary>
+        /// <param name="position">The position around which to search for mines.</param>
+        /// <returns>The amount of mines surrounding the given position.</returns>
+        public byte GetSurroundingMines(int[] position)
+        {
+            byte surroundingMines = 0;
+
+            int[] lowerLimits = new int[3];
+            for (int i = 0; i < lowerLimits.Length; i++)
+            {
+                lowerLimits[i] = position[i] - 1 < 0 ? 0 : position[i] - 1;
+            }
+
+            int[] upperLimits = new int[3];
+            upperLimits[0] = position[0] + 1 >= Width ? Width : position[0] + 2;
+            upperLimits[1] = position[1] + 1 >= Height ? Height : position[1] + 2;
+            upperLimits[2] = position[2] + 1 >= Depth ? Depth : position[2] + 2;
+
+
+            for (int z = lowerLimits[2]; z < upperLimits[2]; z++)
+            {
+                for (int y = lowerLimits[1]; y < upperLimits[1]; y++)
+                {
+                    for (int x = lowerLimits[0]; x < upperLimits[0]; x++)
+                    {
+                        if (x == 0 && y == 0 && z == 0)
+                            continue;
+
+                        if (Cubes[position[0] + x, position[1] + y, position[2] + z].HasMine)
+                            surroundingMines++;
+                    }
+                }
+            }
+
+            return surroundingMines;
+        }
+
+        /// <summary>
+        /// Gets the amount of mines surrounding a given <see cref="Cube"/>.
+        /// </summary>
+        /// <param name="cube">The <see cref="Cube"/> around which to search for mines.</param>
+        /// <returns>The amount of mines surrounding the given <see cref="Cube"/>.</returns>
+        public byte GetSurroundingMines(Cube cube)
+        {
+            int[] cubePosition;
+
+            if (Cubes[cube.Position[0], cube.Position[1], cube.Position[2]] == cube)
+                cubePosition = cube.Position;
+            else
+                cubePosition = GetCubePosition(cube);
+
+            return GetSurroundingMines(cubePosition);
+        }
+
+        #endregion
+    }
+}
