597ff6917327584397412c6a1fef38442d8a5c11
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2021-10-16 17:57:36 +0200

    Add documentation to each enum possibility

--- Library/Source/Enums.cs
+++ Library/Source/Enums.cs
@@ -3,15 +3,73 @@
     /// <summary>
     /// Contains all acceptable states of a <see cref="Cube"/>.
     /// </summary>
-    public enum CubeState { Covered, Uncovered, Flagged }
+    public enum CubeState
+    {
+        /// <summary>
+        /// Specifies a covered (not yet uncovered) cube.
+        /// </summary>
+        Covered,
+
+        /// <summary>
+        /// Specifies an uncovered cube.
+        /// </summary>
+        Uncovered,
+
+        /// <summary>
+        /// Specifies a cube that contains a flag.
+        /// </summary>
+        Flagged
+    }
 
     /// <summary>
     /// Contains all acceptable states of a <see cref="Game"/>.
     /// </summary>
-    public enum GameStatus { NotStarted, Ongoing, Lost, Won }
+    public enum GameStatus
+    {
+        /// <summary>
+        /// Specifies a game that hasn't started yet.
+        /// </summary>
+        NotStarted,
+
+        /// <summary>
+        /// Specifies a game that is ongoing.
+        /// </summary>
+        Ongoing,
+
+        /// <summary>
+        /// Specifies a game that ended in a loss for the player (who stepped on a mine)
+        /// </summary>
+        Lost,
+
+        /// <summary>
+        /// Specifies a game that ended in a player's victory.
+        /// </summary>
+        Won
+    }
 
     /// <summary>
     /// Contains all possible results of a cube uncovering action.
     /// </summary>
-    public enum UncoveringResult { Mine, Clear, Flag }
+    public enum UncoverResult
+    {
+        /// <summary>
+        /// The uncovered <see cref="Cube"/> was mined.
+        /// </summary>
+        Mine,
+        
+        /// <summary>
+        /// The uncovered <see cref="Cube"/> was clear, didn't contain any mine.
+        /// </summary>
+        Clear,
+
+        /// <summary>
+        /// The uncovering operation was stopped because the <see cref="Cube"/> was <see cref="CubeState.Flagged"/>.
+        /// </summary>
+        Flag,
+        
+        /// <summary>
+        /// The uncovering operation was stopped because the <see cref="Cube"/> was already <see cref="Uncovered"/>.
+        /// </summary>
+        Uncovered
+    }
 }
